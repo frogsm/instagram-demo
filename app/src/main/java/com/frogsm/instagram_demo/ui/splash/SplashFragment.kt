d8c580cfb0d3d9f94816592f3338062afabfe9a3
frogsm <frogsm@estsoft.com>
2020-06-06 16:30:34 +0900

    뷰모델 선언할 수 있도록 수정

--- app/src/main/java/com/frogsm/instagram_demo/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/frogsm/instagram_demo/ui/splash/SplashFragment.kt
@@ -2,10 +2,18 @@ package com.frogsm.instagram_demo.ui.splash
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.frogsm.instagram_demo.R
+import com.frogsm.instagram_demo.ui.ViewModelFactory
 import com.frogsm.instagram_demo.ui.base.BaseFragment
+import javax.inject.Inject
 
 class SplashFragment : BaseFragment(R.layout.fragment_splash) {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by viewModels<SplashViewModel> { viewModelFactory }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
