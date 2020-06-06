8182557edc93951458325d9f2fe78093ca71ad50
frogsm <frogsm@estsoft.com>
2020-06-06 19:21:26 +0900

    액세스토큰 가져오는 API 추가

--- app/src/main/java/com/frogsm/instagram_demo/data/api/AuthorizationApi.kt
+++ app/src/main/java/com/frogsm/instagram_demo/data/api/AuthorizationApi.kt
@@ -1,5 +1,17 @@
 package com.frogsm.instagram_demo.data.api
 
+import com.frogsm.instagram_demo.data.token.AccessTokenData
+import retrofit2.http.POST
+import retrofit2.http.Query
+
 interface AuthorizationApi {
 
+    @POST("/oauth/access_token")
+    suspend fun getAccessToken(
+        @Query("client_id") clientId: String,
+        @Query("client_secret") clientSecret: String,
+        @Query("code") code: String,
+        @Query("grant_type") grantType: String,
+        @Query("redirect_uri") redirectUri: String
+    ): AccessTokenData
 }
