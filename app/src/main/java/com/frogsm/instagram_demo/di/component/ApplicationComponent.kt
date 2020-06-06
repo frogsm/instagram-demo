95fd742d36236881bb00e22df4cc091fb83dd7ef
frogsm <frogsm@estsoft.com>
2020-06-06 15:47:49 +0900

    애플리케이션 추가

---
+++ app/src/main/java/com/frogsm/instagram_demo/di/component/ApplicationComponent.kt
@@ -0,0 +1,22 @@
+package com.frogsm.instagram_demo.di.component
+
+import android.content.Context
+import com.frogsm.instagram_demo.InstagramApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class
+    ]
+)
+interface ApplicationComponent : AndroidInjector<InstagramApplication> {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance applicationContext: Context): ApplicationComponent
+    }
+}
