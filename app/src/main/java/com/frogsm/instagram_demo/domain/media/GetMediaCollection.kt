a3a1f1b104bd6867b87aead8cd4a2f1585695d99
frogsm <frogsm@estsoft.com>
2020-06-09 08:41:37 +0900

    미디어 컬렉션 정보 가져오는 유스케이스 추가

---
+++ app/src/main/java/com/frogsm/instagram_demo/domain/media/GetMediaCollection.kt
@@ -0,0 +1,20 @@
+package com.frogsm.instagram_demo.domain.media
+
+import com.frogsm.instagram_demo.data.media.MediaRepository
+import com.frogsm.instagram_demo.domain.entity.MediaCollection
+import com.frogsm.instagram_demo.domain.usecase.SuspendUseCase
+import javax.inject.Inject
+
+class GetMediaCollection @Inject constructor(
+    private val mediaRepository: MediaRepository
+) : SuspendUseCase<Unit, MediaCollection> {
+
+    override suspend fun invoke(param: Unit): Result<MediaCollection> = try {
+        val mediaCollection = mediaRepository.getMediaCollection()
+
+        Result.success(mediaCollection)
+    } catch (throwable: Throwable) {
+        throwable.printStackTrace()
+        Result.failure(throwable)
+    }
+}
