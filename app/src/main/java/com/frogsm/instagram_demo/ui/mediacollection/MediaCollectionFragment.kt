34b60abcf38567167a5d05ac14ded235294c0776
frogsm <frogsm@estsoft.com>
2020-06-10 01:35:45 +0900

    item decoration 추가

--- app/src/main/java/com/frogsm/instagram_demo/ui/mediacollection/MediaCollectionFragment.kt
+++ app/src/main/java/com/frogsm/instagram_demo/ui/mediacollection/MediaCollectionFragment.kt
@@ -1,13 +1,16 @@
 package com.frogsm.instagram_demo.ui.mediacollection
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.frogsm.instagram_demo.R
 import com.frogsm.instagram_demo.extensions.showLongSnackBar
+import com.frogsm.instagram_demo.extensions.toDp
 import com.frogsm.instagram_demo.ui.ViewModelFactory
 import com.frogsm.instagram_demo.ui.base.BaseFragment
 import com.frogsm.instagram_demo.ui.mediacollection.list.MediaCollectionAdapter
@@ -38,6 +41,23 @@ class MediaCollectionFragment : BaseFragment(R.layout.fragment_media_collection)
             adapter = mediaCollectionAdapter
             layoutManager = GridLayoutManager(requireContext(), 3)
             itemAnimator = null
+
+            addItemDecoration(object : RecyclerView.ItemDecoration() {
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    val space = 10.toDp
+                    val position = parent.getChildAdapterPosition(view)
+                    when (position % 3) {
+                        0 -> outRect.set(0, space, space, space)
+                        1 -> outRect.set(space, space, space, space)
+                        2 -> outRect.set(space, space, 0, space)
+                    }
+                }
+            })
         }
     }
 
