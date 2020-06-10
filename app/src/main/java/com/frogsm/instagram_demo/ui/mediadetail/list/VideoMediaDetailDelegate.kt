5f7c2e60f1842289791948c0ff4f83ab29fa064e
frogsm <frogsm@estsoft.com>
2020-06-11 02:14:18 +0900

    비디오 재생되도록 exo player 추가

--- app/src/main/java/com/frogsm/instagram_demo/ui/mediadetail/list/VideoMediaDetailDelegate.kt
+++ app/src/main/java/com/frogsm/instagram_demo/ui/mediadetail/list/VideoMediaDetailDelegate.kt
@@ -1,6 +1,15 @@
 package com.frogsm.instagram_demo.ui.mediadetail.list
 
+import android.net.Uri
 import android.view.View
+import androidx.core.view.isGone
+import com.frogsm.instagram_demo.R
+import com.google.android.exoplayer2.ExoPlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import kotlinx.android.synthetic.main.item_media_detail_video.view.*
 import javax.inject.Inject
 
 interface VideoMediaDetailDelegate {
@@ -11,6 +20,34 @@ class VideoMediaDetailDelegateImpl @Inject constructor(
 ) : VideoMediaDetailDelegate {
 
     override fun initUi(item: MediaChildrenItem.Video, view: View) {
+        with(view) {
+            val dataSourceFactory =
+                DefaultDataSourceFactory(context, context.getString(R.string.app_name))
 
+            val mediaSource =
+                ProgressiveMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(Uri.parse(item.mediaUrl))
+
+            playerView.apply {
+                useController = false
+                controllerAutoShow = false
+
+                player = SimpleExoPlayer.Builder(context).build().also {
+                    it.playWhenReady = true
+                    it.repeatMode = Player.REPEAT_MODE_ALL
+
+                    it.prepare(mediaSource)
+                    it.addListener(object : Player.EventListener {
+                        override fun onPlayerError(error: ExoPlaybackException) {
+                            when (error.type) {
+                                ExoPlaybackException.TYPE_SOURCE -> {
+                                    playerView.isGone = true
+                                }
+                            }
+                        }
+                    })
+                }
+            }
+        }
     }
 }
