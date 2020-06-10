5121091edd9de3867fcb5cea2d0961af7e9bb1df
frogsm <frogsm@estsoft.com>
2020-06-11 08:53:58 +0900

    paging nullable 변경

--- data/src/main/java/com/frogsm/instagram_demo/data/media/data/MediaCollectionData.kt
+++ data/src/main/java/com/frogsm/instagram_demo/data/media/data/MediaCollectionData.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class MediaCollectionData(
     val data: List<MediaData>,
-    val paging: PagingData
+    val paging: PagingData?
 )
